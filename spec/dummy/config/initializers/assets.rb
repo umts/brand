dff8ae4213728fc0333d37611c20fe513219bad8
benmelz <ben@melz.me>
2026-09-22 16:50:08 -0400

    add fontawesome

--- spec/dummy/config/initializers/assets.rb
+++ spec/dummy/config/initializers/assets.rb
@@ -3,5 +3,6 @@
 Rails.application.configure do
   config.after_initialize do
     config.assets.paths << Rails.root.join('../../node_modules/@fontsource/public-sans/files')
+    config.assets.paths << Rails.root.join('../../node_modules/@fortawesome/fontawesome-free/webfonts')
   end
 end
