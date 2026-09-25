7b399fd05ea03f4e49c77fe154397409114e1840
benmelz <ben@melz.me>
2026-09-25 12:11:24 -0400

    small styling tweak

--- app/helpers/umts/brand/favicon_helper.rb
+++ app/helpers/umts/brand/favicon_helper.rb
@@ -5,11 +5,9 @@ module UMTS
     module FaviconHelper
       def umts_brand_favicons
         Brand.protect_university_trademarks!
-        safe_join([
-                    favicon_link_tag('umts/brand/university-collegiate-m.png', rel: 'icon', type: 'image/png'),
-                    favicon_link_tag('umts/brand/university-collegiate-m.svg', rel: 'icon', type: 'image/svg+xml'),
-                    favicon_link_tag('umts/brand/university-collegiate-m.png', rel: 'apple-touch-icon', type: nil)
-                  ])
+        safe_join([favicon_link_tag('umts/brand/university-collegiate-m.png', rel: 'icon', type: 'image/png'),
+                   favicon_link_tag('umts/brand/university-collegiate-m.svg', rel: 'icon', type: 'image/svg+xml'),
+                   favicon_link_tag('umts/brand/university-collegiate-m.png', rel: 'apple-touch-icon', type: nil)])
       end
     end
   end
